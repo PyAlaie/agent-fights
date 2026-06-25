4ee8c8756ce5e49381d0c280e43b18f48f3c0802
PyAlaie <arshiaalaie1@gmail.com>
2026-06-25 20:00:41 +0330

    added gameResult model

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Env, Agent, Game
+from .models import Env, Agent, Game, GameResult
 
 admin.site.register(Env)
 admin.site.register(Agent)
 admin.site.register(Game)
+admin.site.register(GameResult)
